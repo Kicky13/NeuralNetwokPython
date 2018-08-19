635353cee46f8d31355b12daa8a003557a6b6aec
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2018-08-19 16:47:35 +0700

    update a

--- a.py
+++ a.py
@@ -2,12 +2,12 @@ import NeuralNetwork
 import Neuron
 import NeuronLayer
 
-a = NeuralNetwork.NeuralNetwork(2,2,2)
-a.setHiddenLayerWeight([[0.15,0.2],[0.25,0.3]])
-a.setOutputLayerWeight([[0.4,0.45],[0.5,0.55]])
-a.feedForward([0.05,0.1])
-a.getErrorTotal([0.01,0.99])
-a.derivativeOutputToHidden()
+# a = NeuralNetwork.NeuralNetwork(2, 2, 2)
+# a.setHiddenLayerWeight([[0.15, 0.2], [0.25, 0.3]])
+# a.setOutputLayerWeight([[0.4, 0.45], [0.5, 0.55]])
+# a.feedForward([0.05, 0.1])
+# a.getErrorTotal([0.01, 0.99])
+# a.derivativeOutputToHidden()
 # a.derivativeOutputToInput()
 # b = Neuron.Neuron(0.35)
 # b.setWeights([0.15,0.2])
@@ -18,3 +18,17 @@ a.derivativeOutputToHidden()
 # c = NeuronLayer.NeuronLayer(2,0.35)
 # c.setWeights([[0.15,0.2],[0.25,0.3]])
 # print(c.feedForward([0.05,0.1]))
+
+
+datasets = [
+    [[0.03, 0.05], [0.75]],
+    [[0.05, 0.01], [0.82]],
+    [[0.1, 0.02], [0.93]],
+]
+nn = NeuralNetwork.NeuralNetwork(3, 1, 2)
+nn.setHiddenLayerWeight([[0.84,0.58],[0.04,0.22],[0.13,0.06]])
+nn.setOutputLayerWeight([[0.72,0.73,0.75]])
+for dataset in datasets:
+    nn.addDataset(dataset)
+
+nn.trainingDataset()
