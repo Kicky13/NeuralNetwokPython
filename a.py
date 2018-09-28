38e78dfb5c9effa5d22aff0aa880139a2770dd2d
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2018-09-28 22:07:08 +0700

    change to snake_case

--- a.py
+++ a.py
@@ -1,22 +1,29 @@
+#!/usr/bin/pythonpyt
+
 import NeuralNetwork
 import sys
+sys.path.insert(0, "/home/blegoh/.local/lib/python2.7/site-packages")
 import json
+import Dataset
 
-print sys.argv[1]
 datasets = [
     [[0.03, 0.05], [0.75]],
     [[0.05, 0.01], [0.82]],
     [[0.1, 0.02], [0.93]],
 ]
-nn = NeuralNetwork.NeuralNetwork(3, 1, 2)
-#wh = [[0.84,0.58],[0.04,0.22],[0.13,0.06]]
-wh = json.loads(sys.argv[1])
-print type(wh)
-nn.setHiddenLayerWeight(wh)
-#wo = [[0.72,0.73,0.75]]
-wo = json.loads(sys.argv[2])
-nn.setOutputLayerWeight(wo)
-#for dataset in datasets:
-#    nn.addDataset(dataset)
+grandma = Dataset.Datasets.select()
 
-#nn.trainingDataset()
+# for p in grandma:
+#    print p.kelas
+nn = NeuralNetwork.NeuralNetwork(3, 1, 2)
+# wh = [[0.84,0.58],[0.04,0.22],[0.13,0.06]]
+wh = json.loads(sys.argv[1])
+# print type(wh)
+nn.set_hidden_layer_weight(wh)
+# wo = [[0.72,0.73,0.75]]
+wo = json.loads(sys.argv[2])
+nn.set_output_layer_weight(wo)
+for dataset in datasets:
+    nn.add_dataset(dataset)
+
+nn.trainingDataset()
