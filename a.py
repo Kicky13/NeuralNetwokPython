8cfe53dde79f9a5a8eb5267cfebefd00db67999c
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2018-09-30 09:02:07 +0700

    update a py

--- a.py
+++ a.py
@@ -1,29 +1,26 @@
 #!/usr/bin/pythonpyt
-
-import NeuralNetwork
+import pymysql.cursors
+from NeuralNetwork import NeuralNetwork
 import sys
-sys.path.insert(0, "/home/blegoh/.local/lib/python2.7/site-packages")
 import json
-import Dataset
+from Dataset import Datasets
 
-datasets = [
-    [[0.03, 0.05], [0.75]],
-    [[0.05, 0.01], [0.82]],
-    [[0.1, 0.02], [0.93]],
-]
-grandma = Dataset.Datasets.select()
+data_set = Datasets.select()
 
-# for p in grandma:
-#    print p.kelas
-nn = NeuralNetwork.NeuralNetwork(3, 1, 2)
-# wh = [[0.84,0.58],[0.04,0.22],[0.13,0.06]]
+nn = NeuralNetwork(4, 1, 4)
+
 wh = json.loads(sys.argv[1])
-# print type(wh)
 nn.set_hidden_layer_weight(wh)
-# wo = [[0.72,0.73,0.75]]
+
 wo = json.loads(sys.argv[2])
 nn.set_output_layer_weight(wo)
-for dataset in datasets:
-    nn.add_dataset(dataset)
+
+for data in data_set:
+    inputs = [float(data.asal_smp), int(data.lebih_tua), float(data.jurusan), float(data.pekerjaan_ortu)]
+    if data.kelas == 'nakal':
+        output = [1]
+    else:
+        output = [0]
+    nn.add_dataset([inputs, output])
 
 nn.training_dataset()
